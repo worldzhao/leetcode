b861dda6b2bb6d93a031070bfb257a19ba3fcb83
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2020-03-25 10:03:28 +0800

    update: 翻转二叉树

--- src/leetcode/editor/cn/226.翻转二叉树.js
+++ src/leetcode/editor/cn/226.翻转二叉树.js
@@ -1,33 +1,32 @@
 /**
  * [226].翻转二叉树-invert-binary-tree
  */
- 
-//翻转一棵二叉树。 
+
+// 翻转一棵二叉树。
 //
-// 示例： 
+// 示例：
 //
-// 输入： 
+// 输入：
 //
 //      4
 //   /   \
 //  2     7
 // / \   / \
-//1   3 6   9 
+//1   3 6   9
 //
-// 输出： 
+// 输出：
 //
 //      4
 //   /   \
 //  7     2
 // / \   / \
-//9   6 3   1 
+//9   6 3   1
 //
-// 备注: 
-//这个问题是受到 Max Howell 的 原问题 启发的 ： 
+// 备注:
+// 这个问题是受到 Max Howell 的 原问题 启发的 ：
 //
-// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
 // Related Topics 树
-
 
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
@@ -41,7 +40,22 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
-
+var invertTree = function (root) {
+  if (!root) return null;
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop();
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+    if (node.right) {
+      stack.push(node.right);
+    }
+    if (node.left) {
+      stack.push(node.left);
+    }
+  }
+  return root;
 };
+
 //leetcode submit region end(Prohibit modification and deletion)
