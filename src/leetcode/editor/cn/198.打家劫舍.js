d5fa30477720f41a8bfd399efcade1ff1373bc03
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2020-06-09 14:51:30 +0800

    perf: 动态规划-打家劫舍-使用变量替代数组

--- src/leetcode/editor/cn/198.打家劫舍.js
+++ src/leetcode/editor/cn/198.打家劫舍.js
@@ -54,11 +54,14 @@ var rob = function (nums) {
   if (len <= 0) return 0;
   if (len === 1) return nums[0];
 
-  const dp = [nums[0], Math.max(nums[0], nums[1])];
+  let previous1 = Math.max(nums[0], nums[1]);
+  let previous2 = nums[0];
   for (let i = 2; i < len; i++) {
-    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    let current = Math.max(previous1, previous2 + nums[i]);
+    previous2 = previous1;
+    previous1 = current;
   }
 
-  return dp[len - 1];
+  return previous1;
 };
 //leetcode submit region end(Prohibit modification and deletion)
