59601db015616a3174a1f74f574c0f734c254ffc
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2020-06-09 14:08:42 +0800

    update: 动态规划-打家劫舍

---
+++ src/leetcode/editor/cn/198.打家劫舍.js
@@ -0,0 +1,70 @@
+/**
+ * [198].打家劫舍-house-robber
+ */
+
+// 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上
+// 被小偷闯入，系统会自动报警。
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+//
+//
+//
+// 示例 1：
+//
+// 输入：[1,2,3,1]
+// 输出：4
+// 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+//     偷窃到的最高金额 = 1 + 3 = 4 。
+//
+// 示例 2：
+//
+// 输入：[2,7,9,3,1]
+// 输出：12
+// 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+//     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= nums.length <= 100
+// 0 <= nums[i] <= 400
+//
+// Related Topics 动态规划
+
+/**
+ * f(0) = nums[0];
+ * f(1) = nums[1];
+ * f(2) = f(0) + nums[2];
+ * f(3) = max(f(0), f(1)) + nums[3];
+ * f(4) = max(f(0), f(1), f(2)) + nums[4];
+ * ...
+ * f(n) =  max(f(0), f(1), f(2), ..., f(n-2)) + nums[n];
+ */
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+  const len = nums.length;
+  if (len <= 0) return 0;
+  if (len === 1) return nums[0];
+
+  const dp = [nums[0], nums[1]];
+  let lastMax = 0;
+  for (let i = 2; i < len; i++) {
+    lastMax = Math.max(dp[i - 2], lastMax);
+    dp[i] = lastMax + nums[i];
+  }
+
+  let res = 0;
+  for (let i = 0; i < dp.length; i++) {
+    res = Math.max(dp[i], res);
+  }
+  return res;
+};
+//leetcode submit region end(Prohibit modification and deletion)
